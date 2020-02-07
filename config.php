c6b488704371a4270c36c7d0a56fc62bf2e321ca
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2020-02-07 14:17:05 +0100

    kommentering

--- config.php
+++ config.php
@@ -3,10 +3,14 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "logplan";
+//The information needed to log into the database
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+//Create a connection to the database
 mysqli_set_charset($conn,"utf8");
+//Set the characterset to utf-8
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+//In case of failed connection
 ?>
